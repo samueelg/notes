874c4fa1173f965211c79f5c0134584424f6328c
Samuel <samuel.aluizio@hotmail.com>
2025-04-15 23:53:56 -0300

    :sparkles: create user and note models

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote(){
